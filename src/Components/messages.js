bfbfcd87dcf8e9563c5d3d69561b0126dfedfb43
Pablo Sánchez <pablosanchezflores@gmail.com>
2021-03-26 21:19:16 +0100

    actualizados mensajes

---
+++ src/Components/messages.js
@@ -0,0 +1,11 @@
+export let messages = [
+    { title: "Bien hecho", message: "¿Otra?" },
+
+    { title: "Buen trabajo", message: "Inténtalo otra vez" },
+
+    {
+        title: "Has estado rápido",
+        message: "¿Puedes jugar más rápido?",
+    },
+    { title: "¡Bien!", message: "Has ido rápido." },
+];
